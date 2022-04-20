7e914666233be693dea214d2ac05545f773458c4
paruljamwal <paruljamwal2000@gmail.com>
2022-04-20 17:48:04 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
 import './App.css'
-// import { Button } from './components/Button'
+import { Button } from './components/Button'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-     <Button variant="contained">Primary Button</Button>
-     <Button>Default Button</Button>
-      <Button variant="doated">Dashed Button</Button>
+     <Button variant="primary">Primary Button</Button>
+     <Button variant="default">Default Button</Button>
+      <Button variant="dashed">Dashed Button</Button>
       <Button variant="text">Text Button</Button>
-      <Button variant="outlined" href="#outlined-buttons">Link Button</Button>
+      <Button variant="link">Link Button</Button>
     </div>
   )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,19 +5,27 @@ import styled from 'styled-components';
  const Button=styled.button
  `
  background-color: none;
- border: none;
+ border:${(prop)=>(
+    prop.variant==="default" ? "1px solid grey" : prop.variant ==="dashed" ? "1px dashed grey" : "none"
+ )};
  border-radius: 4px;
  padding:15px;
  padding-top: 7px;
- background-color:skyblue;
- color:white;
+ background-color:${(prop)=>(
+    prop.variant==="primary" ? "#0086ff" : "white"
+ )};
+ color:${(prop)=>(
+    prop.variant==="primary" ? "white" : prop.variant ==="link" ? "blue" :  "black"
+    )};
  font-weight: bolder;
  font-size: larger;
  margin: 4px;
  cursor: pointer;
 
  &:hover{
-    box-shadow :rgba(253,76,0,0.5) 0 3px 8px
+   box-shadow:${(prop)=>(
+      (prop.variant==="link" || prop.variant==="text" )? "none" : "rgba(0, 0, 0, 0.35) 0px 5px 15px;"
+   )
  }
  `
  export {Button}
